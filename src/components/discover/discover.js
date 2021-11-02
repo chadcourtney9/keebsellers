dff4eb8bafadd6986b8d0c9ef6ab9ba0cf3318aa
Chad Courtney <Chadcourtney567@gmail.com>
2021-11-02 14:50:33 -0400

    added discover page

---
+++ src/components/discover/discover.js
@@ -0,0 +1,15 @@
+import react from "react";
+import "./discover.css";
+
+
+function DiscoverPage() {
+
+
+    return (
+        <div>
+            <h1 className="discoverPage-header">Discover</h1>
+        </div>
+    )
+}
+
+export default DiscoverPage;
